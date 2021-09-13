d8dcb8539298d177ec5c860c52bceb663ca55bab
Vitaly Murzin <adn4ops@gmail.com>
2021-09-13 11:51:26 +0500

    modify start_postgres

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop postgres_srv
 docker rm postgres_srv
-docker run -d -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=user -e POSTGRES_DB=netology -v /DATABASE/postgres-data:/var/lib/postgresql/data --name postgres_srv postgres
+docker run -d -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=user -v /DATABASE/postgres-data:/var/lib/postgresql/data -v /DATABASE/postgres-backup:/mnt --name postgres_srv postgres:latest
